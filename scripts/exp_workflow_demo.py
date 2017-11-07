6f05a7e6e90e3185e49f260c83eab2775f46b49d
Seiji Matsuoka <mojaie@aol.com>
2017-11-07 23:34:04 +0900

    WIP5

--- scripts/exp_workflow_demo.py
+++ scripts/exp_workflow_demo.py
@@ -1,12 +1,11 @@
 
 import glob
-import os
-import yaml
 
 from tornado.ioloop import IOLoop
 
 from kiwiii.core.workflow import Workflow
 from kiwiii.node.function.apply import Apply
+from kiwiii.node.function.number import Number
 from kiwiii.node.io.sqlitewriter import SQLiteWriter
 from kiwiii.node.io.csv import CSVFileInput
 from kiwiii.node.record.merge import MergeRecords
@@ -53,11 +52,25 @@ class ExperimentDataDemo(Workflow):
         es = []
         for in_file in glob.glob("./raw/exp_results_demo/*.txt"):
             e1, = self.add_node(CSVFileInput(in_file, delimiter="\t"))
-            e2, = self.add_node(Stack(e1, "id"))
-            e3, = self.add_node(Apply(e2, datatype))
-            es.append(e3)
-        merged, = self.add_node(MergeRecords(es))
-        self.add_node(SQLiteWriter([merged], self, self.params["file"]))
+            e2, = self.add_node(Stack(e1, ["compoundID"]))
+            es.append(e2)
+        es1, = self.add_node(MergeRecords(es))
+        # TODO: delete '_field' field
+        es2, = self.add_node(Apply(
+            es1, datatype,
+            fields=[
+                {"key": "assayID"},
+                {"key": "field"},
+                {"key": "valueType"}
+            ],
+            params={
+                "id": "exp_results", "table": "RESULTS",
+                "name": "Experiment results",
+                "description": "Demo dataset"
+            }
+        ))
+        es3, = self.add_node(Number(es2, field={"key": "id"}))
+        self.add_node(SQLiteWriter([es3], self, self.params["file"]))
 
 
 if __name__ == '__main__':
