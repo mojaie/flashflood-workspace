76290f352a3656f6f92c5f469d7505caf0e4660d
Seiji Matsuoka <mojaie@aol.com>
2018-02-26 18:11:09 +0900

    url prefix option

--- app.py
+++ app.py
@@ -21,33 +21,35 @@ def run():
         "instance": "".join((instance_prefix, timestamp))
     }
     wpath = {True: conf.WEB_BUILD, False: conf.WEB_DIST}[options.debug]
-    app = web.Application(
-        [
-            (r"/search", handler.ChemDBSearch),
-            (r"/filter", handler.ChemDBFilter, params),
-            (r"/profile", handler.Profile),
-            (r"/exact", handler.ExactStruct),
-            (r"/substr", handler.Substruct, params),
-            (r"/supstr", handler.Superstruct, params),
-            (r"/gls", handler.GLS, params),
-            (r"/rdmorgan", handler.RDKitMorgan, params),
-            (r"/rdfmcs", handler.RDKitFMCS, params),
-            (r"/glsnet", handler.GLSNetwork, params),
-            (r"/rdmorgannet", handler.RDKitMorganNetwork, params),
-            (r"/rdfmcsnet", handler.RDKitFMCSNetwork, params),
-            (r"/progress", handler.WorkflowProgress, params),
-            (r"/strprev", handler.StructurePreview),
-            (r"/sdfin", handler.SDFileParser),
-            (r"/sdfout", handler.SDFileExport),
-            (r"/xlsx", handler.ExcelExport),
-            (r"/schema", handler.Schema),
-            (r"/server", handler.ServerStatus, params),
-            (r'/(.*)', web.StaticFileHandler, {"path": wpath})
-        ],
+    handlers = [
+        (r"/search", handler.ChemDBSearch),
+        (r"/filter", handler.ChemDBFilter, params),
+        (r"/profile", handler.Profile),
+        (r"/exact", handler.ExactStruct),
+        (r"/substr", handler.Substruct, params),
+        (r"/supstr", handler.Superstruct, params),
+        (r"/gls", handler.GLS, params),
+        (r"/rdmorgan", handler.RDKitMorgan, params),
+        (r"/rdfmcs", handler.RDKitFMCS, params),
+        (r"/glsnet", handler.GLSNetwork, params),
+        (r"/rdmorgannet", handler.RDKitMorganNetwork, params),
+        (r"/rdfmcsnet", handler.RDKitFMCSNetwork, params),
+        (r"/progress", handler.WorkflowProgress, params),
+        (r"/strprev", handler.StructurePreview),
+        (r"/sdfin", handler.SDFileParser),
+        (r"/sdfout", handler.SDFileExport),
+        (r"/xlsx", handler.ExcelExport),
+        (r"/schema", handler.Schema),
+        (r"/server", handler.ServerStatus, params),
+        (r"/(.*)", web.StaticFileHandler, {"path": wpath})
+    ]
+    hs = [("".join([conf.URL_PREFIX, h[0]]), *h[1:]) for h in handlers]
+    settings = dict(
         debug=options.debug,
         compress_response=True,
         cookie_secret="_TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE_"
     )
+    app = web.Application(hs, **settings)
     app.listen(options.port)
     try:
         print("Server started")
